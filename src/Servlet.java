96df0dd09adf575be95e7487eaca129e5f26ef62
HWON0720 <hwon0720@gmail.com>
2022-05-24 16:27:25 +0900

    add Servlet

---
+++ src/Servlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
